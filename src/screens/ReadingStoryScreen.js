3088e8f2c9b034f8b4a31b6455b6980007729501
MinhTran <tminh400@gmail.com>
2023-09-05 16:28:37 +0700

    Update ReadingStoryScreen.js
    fix clearRect()
    
    - clearTimeout() when start touching

--- src/screens/ReadingStoryScreen.js
+++ src/screens/ReadingStoryScreen.js
@@ -41,14 +41,17 @@ const ReadingStoryScreen = props => {
     `${uri}/api/pages/${pageId}?embed=texts`,
     responsePayload,
   );
+  let timeoutId;
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: (event, gestureState) => {
+      console.log('Touch started at:', gestureState.x0, gestureState.y0);
       const drawText = async () => {
         const canvas = canvasRef.current;
         if (canvas) {
           const ctx = canvas.getContext('2d');
           ctx.font = '16px serif';
+          clearTimeout(timeoutId);
           ctx.clearRect(0, 0, width, height);
           ctx.fillText(
             data.text_configs[1].text.text,
@@ -60,17 +63,41 @@ const ReadingStoryScreen = props => {
           const textMetrics = await ctx.measureText(
             data.text_configs[1].text.text,
           );
-          console.log(textMetrics);
-          setTimeout(() => {
+          timeoutId = setTimeout(() => {
             const textX = x;
             const textY = y;
             const textWidth = textMetrics.width;
-            const textHeight = 30;
-            ctx.clearRect(textX, textY - 20, textWidth, textHeight);
+            const textHeight = 36;
+            ctx.clearRect(textX - 6, textY - 25, textWidth + 12, textHeight);
           }, 3000);
+          function strokeRoundRect(x, y, width, height, radius) {
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.arcTo(x + width, y, x + width, y + radius, radius);
+            ctx.lineTo(x + width, y + height - radius);
+            ctx.arcTo(
+              x + width,
+              y + height,
+              x + width - radius,
+              y + height,
+              radius,
+            );
+            ctx.lineTo(x + radius, y + height);
+            ctx.arcTo(x, y + height, x, y + height - radius, radius);
+            ctx.lineTo(x, y + radius);
+            ctx.arcTo(x, y, x + radius, y, radius);
+            ctx.closePath();
+            ctx.stroke();
+          }
+          // Example usage
+          strokeRoundRect(x - 5, y - 24, textMetrics.width + 10, 32, 10);
         }
       };
       // Handle touch start event
+      // data.text_configs.map((text, index) => {
+      //   console.log(text);
+      // });
       drawText();
     },
     onPanResponderMove: (event, gestureState) => {
@@ -81,32 +108,21 @@ const ReadingStoryScreen = props => {
       // Handle touch end event
       console.log('Touch released at:', gestureState.moveX, gestureState.moveY);
     },
+    //TODO:impl swipe to forward or back page
   });
   const backgroundRef = useRef(null);
   const canvasRef = useRef(null);
   useEffect(() => {
-    const drawPage = async canvas => {
-      const ctx = canvas.getContext('2d');
-      const textData = data.text_configs[contentIndex];
-      console.log(canvas.width, canvas.height);
-      const image = new CanvasImage(canvas);
-      image.src = data.background;
-      image.addEventListener('load', () => {
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#000';
-        ctx.font = '20px Arial';
-        ctx.fillText(textData.text.text, 50, 50);
-        // Draw touchable objects
-        ctx.fillStyle = 'rgba(0,0,0,1)';
-        ctx.fillRect(50, 50, 50, 50);
-      });
-    };
     const drawBackground = async background => {
       const ctx = background.getContext('2d');
       const image = new CanvasImage(background);
+      const textData = data.text_configs[0];
       image.src = data.background;
       image.addEventListener('load', () => {
         ctx.drawImage(image, 0, 0, background.width, background.height);
+        ctx.fillStyle = '#000';
+        ctx.font = '20px Arial';
+        ctx.fillText(textData.text.text, 50, 50);
       });
     };
     const canvas = canvasRef.current;
@@ -118,12 +134,7 @@ const ReadingStoryScreen = props => {
       canvas.height = height;
       drawBackground(backgroundCanvas);
     }
-    // if (canvas) {
-    //   canvas.width = width;
-    //   canvas.height = height;
-    //   drawPage(canvas);
-    // }
-  }, [isLoading, data, contentIndex]);
+  }, [isLoading, data]);
   if (data.text_configs.length === 0) {
     return (
       <Box bg="$white" p="$5">
@@ -142,7 +153,7 @@ const ReadingStoryScreen = props => {
     <Box width="100%" height="100%" {...panResponder.panHandlers}>
       <Canvas
         ref={backgroundRef}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+        style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
       />
       <Canvas ref={canvasRef} style={{width: '100%', height: '100%'}} />
       <Fab
