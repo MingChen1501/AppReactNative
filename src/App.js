802c2fe0fba6d7529d9b076d4e09bcd6ab406d1b
MinhTran <tminh400@gmail.com>
2023-09-11 10:17:47 +0700

    Update App.js
    use Navigator with isLogged

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {config, GluestackUIProvider} from '@gluestack-ui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import UserNavigator from './navigation/UserNavigator';
 
 const App = () => {
+  const isLogged = false;
+  const navigator = isLogged ? <AppNavigator /> : <UserNavigator />;
   return (
     <GluestackUIProvider config={config.theme}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <NavigationContainer>{navigator}</NavigationContainer>
     </GluestackUIProvider>
   );
 };
