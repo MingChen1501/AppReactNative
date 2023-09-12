d0ffa7d71f25e2f16ba7c64ac3ed95043221c71c
MinhTran <tminh400@gmail.com>
2023-09-12 13:51:14 +0700

    Update LoginScreen.js
    handle flow when request login
    - 200 show success and login to the app (develop later)
    - 401 show error login fail
    - validate show warming validate form login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,47 +1,103 @@
 import React, {useState} from 'react';
 import {
   AddIcon,
+  Alert,
+  AlertIcon,
+  AlertText,
   Box,
   Button,
   ButtonIcon,
   ButtonText,
   Center,
   HStack,
+  InfoIcon,
   Input,
   InputInput,
   Spinner,
   Text,
   VStack,
 } from '@gluestack-ui/themed';
+import Aleart from '../components/Aleart';
+import {uri} from '../utils/Host';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
+  const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
+  const [feedback, setFeedback] = useState(null);
+  const loadingComponent = <Spinner size={60} />;
+  const loginRequest = async () => {
+    const request = await fetch(uri + '/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username: username, password: password}),
+    });
+    const response = await request.json();
+    return {httpStatus: request.status, response: response};
+  };
+  const handleSubmit = async () => {
+    setIsUsernameInvalid(false);
+    setIsPasswordInvalid(false);
+    if (username.length < 6 || password.length < 6) {
+      showDialog(
+        {
+          message: 'username and password must be greater than 6 characters',
+          action: 'warning',
+        },
+        presentFormLogin,
+      );
+      return;
+    }
+    setFeedback(loadingComponent);
+    const loginState = await loginRequest();
+    if (loginState.httpStatus === 200) {
+      showDialog({
+        message: 'login successful, waiting 5 seconds',
+        action: 'success',
+      });
+    } else if (loginState.httpStatus === 401) {
+      showDialog({message: 'username or password is wrong', action: 'error'});
+    }
+  };
+  const showDialog = (
+    {message = 'unknown diaglog', action = 'info'},
+    callback,
+  ) => {
+    if (typeof callback === 'function') {
+      callback();
+    }
+    setFeedback(Aleart(message, action));
+  };
 
-  const handleSubmit = () => {
-    // Perform login logic using the email and password values
-    // Call an authentication function or dispatch a login action
-    console.log('Email:', email);
-    console.log('Password:', password);
-    setIsLoading(true);
+  const presentFormLogin = () => {
+    setIsUsernameInvalid(username.length < 6);
+    setIsPasswordInvalid(password.length < 6);
   };
   return (
-    <Center h="$80">
+    <Box h="$80" paddingTop={'5%'} alignItems="center">
       <VStack space="md" reversed={false} w="$80">
         <Input
           variant="outline"
           size="md"
           isDisabled={false}
-          isInvalid={false}
+          isInvalid={isUsernameInvalid}
           isReadOnly={false}>
-          <InputInput placeholder="username" onChangeText={setEmail} />
+          <InputInput
+            placeholder="username"
+            onChangeText={text => {
+              setUsername(text);
+              setIsUsernameInvalid(false);
+            }}
+          />
         </Input>
         <Input
           variant="outline"
           size="md"
           isDisabled={false}
-          isInvalid={false}
+          isInvalid={isPasswordInvalid}
           isReadOnly={false}>
           <InputInput
             type="password"
@@ -59,8 +115,8 @@ const LoginScreen = () => {
           <ButtonText>Login</ButtonText>
         </Button>
       </VStack>
-      {isLoading ? <Spinner size={40} color="primary.500" /> : null}
-    </Center>
+      {feedback}
+    </Box>
   );
 };
 export default LoginScreen;
