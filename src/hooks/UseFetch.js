12ef4ec62ccdbf5ecf45af8b408b005e15dc5588
MinhTran <tminh400@gmail.com>
2023-09-13 18:07:02 +0700

    Update UseFetch.js
    delete mock jwt

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,15 +1,15 @@
 import {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 
-const jwt =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNjk0NDAzNDA0LCJleHAiOjE2OTQ0MDcwMDQsIm5iZiI6MTY5NDQwMzQwNCwianRpIjoiV283OXFHczQ1U1hrc1pKMiIsInN1YiI6IjciLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwiMCI6InhtY2tlbnppZSIsIjEiOiJSZXltdW5kbyIsIjIiOiJMYW5nIn0.-9Rzvl1wlHookSL3RJP2Lc9ONSdx_BzJUWEXNSUi9Bw';
-const option = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + jwt,
-  },
-};
 const UseDataFetching = (url, responsePayload) => {
+  const jwt = useSelector(state => state.jwt);
+  const option = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + jwt,
+    },
+  };
   const [data, setData] = useState(responsePayload);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
