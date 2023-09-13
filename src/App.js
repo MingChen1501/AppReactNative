322c2d6d9292bfa1b0246b06aad40d8e99ca9026
MinhTran <tminh400@gmail.com>
2023-09-13 18:11:23 +0700

    Update App.js
    - read isLogged from redux
    - useEffect with dependency isLogged state for reRender application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {config, GluestackUIProvider} from '@gluestack-ui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 import UserNavigator from './navigation/UserNavigator';
+import {useSelector, Provider} from 'react-redux';
+import store from './redux/Store';
 
 const App = () => {
-  const isLogged = false;
+  const isLogged = useSelector(state => state.isLogged);
   const navigator = isLogged ? <AppNavigator /> : <UserNavigator />;
+  useEffect(() => {
+    console.log('isLogged', isLogged);
+  }, [isLogged]);
   return (
     <GluestackUIProvider config={config.theme}>
       <NavigationContainer>{navigator}</NavigationContainer>
     </GluestackUIProvider>
   );
 };
-export default App;
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+export default Root;
