16ac52d8dffbda2636d84b070b5afe6d9bdb3a07
MinhTran <tminh400@gmail.com>
2023-09-20 09:17:34 +0700

    Update ReadingStoryScreen.js
    -separate draw background and text content to 2 useEffect
    - use a state to store Sound reference to play
    - use requestAnimateFrame to create animation syncTextSound, cancel with useRef store id of request

--- src/screens/ReadingStoryScreen.js
+++ src/screens/ReadingStoryScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Box, Fab, FabLabel, Text} from '@gluestack-ui/themed';
+import {Box, Text} from '@gluestack-ui/themed';
 import Sound from 'react-native-sound';
 import useFetchStory from '../hooks/UseFetch';
 import {uri} from '../utils/Host';
@@ -29,20 +29,28 @@ const responsePayload = {
 const ReadingStoryScreen = props => {
   const pages = props.route.params.pages;
   const [page, setPage] = useState({id: pages[0], index: 0});
+  const [isLoadedTextContentAudio, setIsLoadedTextContentAudio] = useState({
+    isLoaded: false,
+    sound: null,
+  });
+  const [isRefreshTextContent, setIsRefreshTextContent] = useState(false);
   const [{data, isLoading, error}, fetchData] = useFetchStory(
     `${uri}/api/pages/${page.id}?embed=texts`,
     responsePayload,
   );
-  console.log('id: ', page.id);
-  console.log('pages: ', pages);
-  console.log('index: ', page.index);
-  let strokes = [];
+  const textRef = useRef(null);
+  const backgroundRef = useRef(null);
+  const canvasRef = useRef(null);
+  const strokes = useRef([]);
+  const animationFrameId = useRef(null);
   let timeoutId;
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: async (_event, gestureState) => {
       const drawText = async state => {
         const canvas = canvasRef.current;
+        canvas.width = width;
+        canvas.height = height;
         if (canvas) {
           //TODO: should be check gestureState position in touchable object position
           if (state.index > -1) {
@@ -61,7 +69,6 @@ const ReadingStoryScreen = props => {
             const x = state.position.x0;
             const y = state.position.y0;
             strokeRoundRect(x - 5, y - 24, textMetrics.width + 10, 32, 10);
-            // sound.play();
             const sound = new Sound(
               data.touchable_objects[state.index].text.audio.url,
               null,
@@ -120,7 +127,9 @@ const ReadingStoryScreen = props => {
           }
         });
       };
-      await drawText(await isPositionInsideStroke(gestureState, strokes));
+      await drawText(
+        await isPositionInsideStroke(gestureState, strokes.current),
+      );
     },
     onPanResponderMove: (event, gestureState) => {},
     onPanResponderRelease: (event, gestureState) => {
@@ -128,90 +137,148 @@ const ReadingStoryScreen = props => {
       const horizontalSwipe = Math.abs(dx) > Math.abs(dy);
       if (horizontalSwipe) {
         const tmp = 0.2 * width;
-        console.log('log', dx, dy);
         if (dx > tmp) {
           if (page.index > 0) {
-            console.log('right');
+            const canvas = textRef.current;
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, width, height);
+            isLoadedTextContentAudio.sound.stop();
+            cancelAnimationFrame(animationFrameId.current);
             setPage({id: pages[page.index - 1], index: page.index - 1});
           }
-          // fetchData();
         } else if (dx < -tmp) {
           if (page.index < pages.length - 1) {
-            console.log('left');
+            const canvas = textRef.current;
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, width, height);
+            isLoadedTextContentAudio.sound.stop();
+            cancelAnimationFrame(animationFrameId.current);
             setPage({id: pages[page.index + 1], index: page.index + 1});
           }
-          // fetchData();
         }
       } else {
         const tmp = 0.4 * height;
         if (dy > tmp) {
-          fetchData();
+          isLoadedTextContentAudio.sound.stop();
+          cancelAnimationFrame(animationFrameId.current);
+          setIsRefreshTextContent(!isRefreshTextContent);
         }
       }
     },
-    //TODO:impl swipe to forward or back page
   });
-  const backgroundRef = useRef(null);
-  const canvasRef = useRef(null);
   useEffect(() => {
     const drawBackground = async background => {
-      strokes = [];
+      strokes.current = [];
       const ctx = background.getContext('2d');
       const image = new CanvasImage(background);
-      const textData = data.text_configs[0];
       image.src = data.background;
+      ctx.clearRect(0, 0, background.width, background.height);
+      const sound = new Sound(data.text_configs[0].text.audio.url, null, e => {
+        if (e) {
+          return;
+        }
+        setIsLoadedTextContentAudio({isLoaded: true, sound: sound});
+      });
       image.addEventListener('load', () => {
         ctx.drawImage(image, 0, 0, background.width, background.height);
-        ctx.fillStyle = '#000';
-        ctx.font = '20px Arial';
-        ctx.fillText(textData.text.text, background.width - 661, 93);
-        if (textData.text?.audio?.url) {
-          const audio = new Sound(
-            data.text_configs[0].text.audio.url,
-            null,
-            () => {
-              audio.play(success => {
-                console.log(audio);
-                if (!success) {
-                  console.log('playback failed due to audio decoding errors');
-                } else {
-                  console.log('successfully finished playing');
-                }
-              });
-            },
-          );
-        }
-        const touchableObjectPosition = data.touchable_objects.map(e => {
-          return JSON.parse(e.position);
+      });
+      const touchableObjectPosition = data.touchable_objects.map(e => {
+        return JSON.parse(e.position);
+      });
+      touchableObjectPosition.map(async (e, index) => {
+        const stroke = new Path2D(ctx);
+        ctx.beginPath();
+        e.forEach(vertice => {
+          const x = (vertice.x * width) / 1700;
+          const y = (vertice.y * height) / 768;
+          if (vertice === e[0]) {
+            stroke.moveTo(x, height - y);
+          } else {
+            stroke.lineTo(x, height - y);
+          }
         });
-        touchableObjectPosition.map(async (e, index) => {
-          const stroke = new Path2D(ctx);
-          ctx.beginPath();
-          e.forEach(vertice => {
-            const x = (vertice.x * width) / 1700;
-            const y = (vertice.y * height) / 768;
-            if (vertice === e[0]) {
-              stroke.moveTo(x, height - y);
-            } else {
-              stroke.lineTo(x, height - y);
-            }
-          });
-          ctx.closePath();
-          strokes.push({ref: stroke, index: index});
-        });
-        console.log(strokes);
+        ctx.closePath();
+        strokes.current.push({ref: stroke, index: index});
       });
     };
-    const canvas = canvasRef.current;
+    const w = width;
+    const h = height;
     const backgroundCanvas = backgroundRef.current;
-    if (backgroundCanvas && canvas) {
-      backgroundCanvas.width = width;
-      backgroundCanvas.height = height;
-      canvas.width = width;
-      canvas.height = height;
+    if (backgroundCanvas) {
+      backgroundCanvas.width = w;
+      backgroundCanvas.height = h;
       drawBackground(backgroundCanvas);
     }
   }, [isLoading, data]);
+  useEffect(() => {
+    const drawTextContent = async textCanvas => {
+      if (!isLoadedTextContentAudio.isLoaded) {
+        return;
+      }
+      isLoadedTextContentAudio.sound.play();
+      const textCtx = textCanvas.getContext('2d');
+      textCtx.font = '20px Arial';
+      const text = data.text_configs[0].text.text;
+      const measureText = await textCtx.measureText(text);
+      const words = text.split(' ');
+      const textPosition = [];
+      for (const word of words) {
+        const measureWord = await textCtx.measureText(word + ' ');
+        textPosition.push(measureWord.width);
+      }
+      const syncTextSounds = JSON.parse(
+        data.text_configs[0].text.audio.sync_text_sound,
+      );
+      const highlightDurations = syncTextSounds.map(item => {
+        const duration = item.te - item.ts;
+        return duration * 100;
+      });
+      let currentIndex = 0;
+      let startTime = null;
+      const animate = () => {
+        const currentTime = Date.now();
+        if (!startTime) {
+          startTime = currentTime;
+        }
+        const elapsedTime = currentTime - startTime;
+        let x = (width - measureText.width) / 2;
+        for (let i = 0; i < words.length; i++) {
+          const word = words[i];
+          const isHighlighted =
+            i === currentIndex &&
+            elapsedTime >= 0 &&
+            elapsedTime < highlightDurations[i];
+          if (isHighlighted) {
+            textCtx.fillStyle = 'red';
+          } else {
+            textCtx.fillStyle = 'black';
+          }
+          textCtx.fillText(word, x, 30);
+          x += textPosition[i];
+        }
+        if (elapsedTime >= highlightDurations[currentIndex]) {
+          currentIndex++;
+          startTime = null;
+        }
+        if (currentIndex < words.length) {
+          cancelAnimationFrame(animationFrameId.current);
+          animationFrameId.current = requestAnimationFrame(animate);
+        } else {
+          textCtx.clearRect(0, 0, width, height);
+          textCtx.fillText(text, (width - measureText.width) / 2, 30);
+          // renderCtx.drawImage(textCanvas, 0, 0);
+        }
+      };
+      animationFrameId.current = requestAnimationFrame(animate);
+    };
+    const textCanvas = textRef.current;
+    if (textCanvas) {
+      textCanvas.width = width;
+      textCanvas.height = height;
+      drawTextContent(textCanvas);
+    }
+    return cancelAnimationFrame(animationFrameId.current);
+  }, [isLoadedTextContentAudio, isRefreshTextContent]);
   if (data.text_configs.length === 0) {
     return (
       <Box bg="$white" p="$5">
@@ -228,12 +295,10 @@ const ReadingStoryScreen = props => {
   }
   return (
     <Box width="100%" height="100%" {...panResponder.panHandlers}>
-      <Canvas
-        ref={backgroundRef}
-        style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
-      />
-      <Canvas ref={canvasRef} style={{width: '100%', height: '100%'}} />
-      <Fab
+      <Canvas ref={backgroundRef} style={{position: 'absolute'}} />
+      <Canvas ref={textRef} style={{position: 'absolute'}} />
+      <Canvas ref={canvasRef} style={{position: 'absolute'}} />
+      {/* <Fab
         size="sm"
         placement="top right"
         isHovered={false}
@@ -241,7 +306,7 @@ const ReadingStoryScreen = props => {
         isPressed={false}
         onPress={() => {}}>
         <FabLabel>Menu</FabLabel>
-      </Fab>
+      </Fab> */}
     </Box>
   );
 };
